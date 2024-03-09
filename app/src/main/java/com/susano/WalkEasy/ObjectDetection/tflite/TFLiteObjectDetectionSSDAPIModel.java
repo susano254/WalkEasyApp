595f1649a14d6e503ed99f5f040236a9747a4e69
nerme <nermeen3nana@gmail.com>
2024-03-09 21:26:32 +0200

    commit my changes

--- app/src/main/java/com/susano/WalkEasy/ObjectDetection/tflite/TFLiteObjectDetectionSSDAPIModel.java
+++ app/src/main/java/com/susano/WalkEasy/ObjectDetection/tflite/TFLiteObjectDetectionSSDAPIModel.java
@@ -41,7 +41,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
-
+// messange
 public class TFLiteObjectDetectionSSDAPIModel extends Classifier {
 
   final static String TAG = "TFLiteObjectDetectionSSDAPIModel";
